0738e408c2c05eb5234cbfad7e282916e08beac0
JonathanDP02 <jonatanpangestu23@gmail.com>
2025-04-18 22:09:02 +0700

    p

--- mataKuliah01.java
+++ mataKuliah01.java
@@ -1,3 +1,15 @@
 public class mataKuliah01 {
-    
+    String kodemk;
+    String namamk;
+    int sks;
+
+    public mataKuliah01(String km, String nm, int sk){
+        km = kodemk;
+        nm = namamk;
+        sk = sks;
+    }
+
+    public void tampilMatakuliah(){
+        System.out.println("Kode MK: " + kodemk + " | Nama: " + namamk + " | SKS: " + sks);
+    }
 }
